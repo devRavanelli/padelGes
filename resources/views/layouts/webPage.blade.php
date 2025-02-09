0fb49f571d90ddb39caa1f47d3ebb616220fa9c9
Sammy Odeh <samodetor@alu.edu.gva.es>
2025-02-09 10:12:51 +0100

    probando 1 2 secure asset

--- resources/views/layouts/webPage.blade.php
+++ resources/views/layouts/webPage.blade.php
@@ -8,9 +8,9 @@
     <title>@yield('title', 'PRV - Pádel Resort Valencia')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/indexMed.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/indexBig.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ secure_asset('css/index.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ secure_asset('css/indexMed.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ secure_asset('css/indexBig.css') }}" type="text/css">
     <link rel="icon" href="{{ asset('assets/logoChatGpt.png') }}" type="image/png">
     @yield('styles') <!-- Para CSS específicos de cada vista -->
 </head>
