aa72e9532312bb35747d0562d2319422643297e0
Sammy Odeh <samodetor@alu.edu.gva.es>
2025-02-09 09:47:35 +0100

    mixed content fix

--- resources/views/layouts/webPage.blade.php
+++ resources/views/layouts/webPage.blade.php
@@ -8,10 +8,10 @@
     <title>@yield('title', 'PRV - Pádel Resort Valencia')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-    <link rel="stylesheet" href="{{ secure_asset('css/index.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ secure_asset('css/indexMed.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ secure_asset('css/indexBig.css') }}" type="text/css">
-    <link rel="icon" href="{{ secure_asset('assets/logoChatGpt.png') }}" type="image/png">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/indexMed.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/indexBig.css') }}" type="text/css">
+    <link rel="icon" href="{{ asset('assets/logoChatGpt.png') }}" type="image/png">
     @yield('styles') <!-- Para CSS específicos de cada vista -->
 </head>
 
@@ -28,8 +28,8 @@
     <!-- Footer -->
     @include('layouts.partials.footer')
 
-    <script src="{{ secure_asset('js/jquery-3.7.1.js') }}"></script>
-    <script src="{{ secure_asset('js/menu.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.7.1.js') }}"></script>
+    <script src="{{ asset('js/menu.js') }}"></script>
     @yield('scripts') <!-- Para JS específicos de cada vista -->
 </body>
 
